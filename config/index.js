b435837008e106f52c1937d67a313a7086aae379
Aditi Garg <aditiga@zillowgroup.com>
2022-01-18 15:09:28 -0800

    fix: path

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 export const config = {
   env: "prod",
   LOCAL_API_URL: "localhost:3000",
-  API_URL: "ndn-rarity.vercel.app",
+  API_URL: "ranking.neardragons.com",
   COLLECTION_NAME: "NDN",
   COLLECTION_TITLE: "Near Dragon Nation",
   COLLECTION_DESCRIPTION: '3,000 Dragons on the NEAR blockchain.',
