b61adbba4ef84832054091d6af264afa257c1922
Aditi Garg <aditiga@zillowgroup.com>
2022-01-18 15:35:56 -0800

    feat: experimental refactor

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { IoMdClose } from 'react-icons/io';
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
 
 function Navbar({ title, menu, showMenu, setShowMenu }) {
   return (
     <div className="bg-gray-800 w-full flex justify-between items-center p-4">
       <h1 className="font-mono text-lg text-white font-medium">{title}</h1>
-      {menu &&
-        <div className="md:hidden text-md text-white" onClick={() => setShowMenu(!showMenu)}>
+      <div
+        className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
+        role="alert"
+      >
+        <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
+          Experimental
+        </span>
+        <span className="font-semibold mr-2 text-left flex-auto">
+          Rankings subject to change
+        </span>
+      </div>
+      {menu && (
+        <div
+          className="md:hidden text-md text-white"
+          onClick={() => setShowMenu(!showMenu)}
+        >
           {showMenu ? <IoMdClose /> : <GiHamburgerMenu />}
         </div>
-      }
+      )}
     </div>
-  )
+  );
 }
 
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -53,17 +53,6 @@ function Home({ title, img, description, nfts, pages, filters }) {
         setShowMenu={setShowMenu}
         showMenu={showMenu}
       />
-        <div
-          class="mt-5 p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
-          role="alert"
-        >
-          <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
-            Experimental
-          </span>
-          <span class="font-semibold mr-2 text-left flex-auto">
-            Rankings subject to change
-          </span>
-        </div>
       <main className="relative flex justify-center w-full flex-1 h-screen">
         <SideBar
           all_traits={all_traits}
