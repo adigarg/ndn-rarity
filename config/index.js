236c1dd4c3be36cb747d035def8dd0eedc62d4a0
Aditi Garg <aditiga@zillowgroup.com>
2022-01-18 14:54:49 -0800

    fix: config

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 export const config = {
-  env: "local",
+  env: "prod",
   LOCAL_API_URL: "localhost:3000",
-  API_URL: "",
+  API_URL: "ndn-rarity.vercel.app",
   COLLECTION_NAME: "NDN",
   COLLECTION_TITLE: "Near Dragon Nation",
   COLLECTION_DESCRIPTION: '3,000 Dragons on the NEAR blockchain.',
